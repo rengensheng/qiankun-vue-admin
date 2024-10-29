352e325c0152c32708a57059ee9752473b5b51e7
chaixz <chaixz@glodon.com>
2024-10-29 17:12:08 +0800

    feat: 代理配置优化

--- vite.config.ts
+++ vite.config.ts
@@ -3,29 +3,6 @@ import UnoCSS from 'unocss/vite'
 import { visualizer } from "rollup-plugin-visualizer";
 import vue from '@vitejs/plugin-vue'
 
-const getProxyUrl = (mode) => {
-  return mode === 'production' ? 'http://learn.rengs.asia' : 'http://localhost:8080'
-}
-// https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [vue(),UnoCSS(), visualizer({
-//     gzipSize: true,
-//     brotliSize: true,
-//     emitFile: false,
-//     filename: "test.html",
-//     open:true
-//   })],
-//   base: "./",
-//   server: {
-//     proxy: {
-//       "/api": {
-//         target: getProxyUrl(),
-//         changeOrigin: true,
-//       },
-//     }
-//   },
-// })
-
 export default defineConfig(({ command, mode }) => {
   // 根据当前工作目录中的 `mode` 加载 .env 文件
   // 设置第三个参数为 '' 来加载所有环境变量，而不管是否有 `VITE_` 前缀。
@@ -42,7 +19,7 @@ export default defineConfig(({ command, mode }) => {
     server: {
       proxy: {
         "/api": {
-          target: getProxyUrl(mode),
+          target: env.VITE_PROXY_URL,
           changeOrigin: true,
         },
       }
