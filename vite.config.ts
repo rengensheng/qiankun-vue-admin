bcd529096db8be18b9cc553623b6fa92611527f6
chaixz <chaixz@glodon.com>
2024-10-29 19:02:39 +0800

    feat: 增加node的类型声明

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import UnoCSS from 'unocss/vite'
 import { visualizer } from "rollup-plugin-visualizer";
 import vue from '@vitejs/plugin-vue'
 
-export default defineConfig(({ command, mode }) => {
+export default defineConfig(({ mode }) => {
   // 根据当前工作目录中的 `mode` 加载 .env 文件
   // 设置第三个参数为 '' 来加载所有环境变量，而不管是否有 `VITE_` 前缀。
   const env = loadEnv(mode, process.cwd(), '')
