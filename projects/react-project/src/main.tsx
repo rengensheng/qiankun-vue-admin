9a01b9a42847fe89e73357147f92978dca1b9159
chaixz <chaixz@glodon.com>
2024-10-30 21:02:11 +0800

    feat: qiankun配置强制样式隔离

--- projects/react-project/src/main.tsx
+++ projects/react-project/src/main.tsx
@@ -2,42 +2,43 @@ import { StrictMode } from 'react'
 import type { Root } from 'react-dom/client'
 import { createRoot } from 'react-dom/client'
 import type { QiankunProps } from 'vite-plugin-qiankun/dist/helper.d.ts'
-import { renderWithQiankun, qiankunWindow } from 'vite-plugin-qiankun/dist/helper';
+import { renderWithQiankun, qiankunWindow } from 'vite-plugin-qiankun/dist/helper'
 import './index.css'
 import App from './App.tsx'
 import '../global.d.ts'
 
-let ReactDOM: Root;
-
+let ReactDOM: Root
 
 function render(props: QiankunProps) {
-  const { container } = props;
-  const containerElement = container ? container : document.querySelector('#root')
+  const { container } = props
+  const containerElement = container
+    ? container.querySelector('#root')
+    : document.querySelector('#root')
   ReactDOM = createRoot(containerElement!)
   ReactDOM.render(
     <StrictMode>
       <App />
-    </StrictMode>,
+    </StrictMode>
   )
 }
 
 renderWithQiankun({
   mount(props: QiankunProps) {
-    console.log('mount');
-    render(props);
+    console.log('mount')
+    render(props)
   },
   bootstrap() {
-    console.log('bootstrap');
+    console.log('bootstrap')
   },
   unmount(props: QiankunProps) {
-    const { container } = props;
-    console.log('unmount', container);
+    const { container } = props
+    console.log('unmount', container)
     ReactDOM.unmount()
   },
   update(props: QiankunProps) {
-    console.log('update', props.container);
+    console.log('update', props.container)
   }
-});
+})
 if (!qiankunWindow.__POWERED_BY_QIANKUN__) {
-  render({});
+  render({})
 }
