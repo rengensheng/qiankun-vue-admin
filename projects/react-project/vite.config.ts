0d1ca9bd2d31afc7a080b7aac617bbe1ba8d18dc
chaixz <chaixz@glodon.com>
2024-10-30 15:48:09 +0800

    feat: 集成react联调

--- projects/react-project/vite.config.ts
+++ projects/react-project/vite.config.ts
@@ -3,7 +3,15 @@ import react from '@vitejs/plugin-react'
 import qiankun from 'vite-plugin-qiankun';
 
 // https://vite.dev/config/
+const useDevMode = true
 export default defineConfig({
-  plugins: [react(), qiankun('sub-react-app', { useDevMode: true }),],
-  base: '/',
+  plugins: [
+    // react(),
+    qiankun('微服务页面2', { useDevMode: useDevMode })
+  ],
+  base: '/micro/sub1/second',
+  server: {
+    port: 5174,
+    origin: 'http://localhost:5174',
+  }
 })
