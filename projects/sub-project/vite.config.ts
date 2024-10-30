55b521420e623120239b46ae1fcb9496f41454c6
rengensheng <805776144@qq.com>
2024-10-30 14:59:56 +0800

    fix: 微服务挂载完善

--- projects/sub-project/vite.config.ts
+++ projects/sub-project/vite.config.ts
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 import qiankun from 'vite-plugin-qiankun';
 
 export default defineConfig({
-  plugins: [vue(), qiankun('分析页', { useDevMode: true }),],
+  plugins: [vue(), qiankun('sub1', { useDevMode: true }),],
   base: '/micro/sub1/',
   server: {
     origin: 'http://localhost:5174'
