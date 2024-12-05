23cda5b0545ea0f0d42fc8671c7858da5266bc2f
rengensheng <805776144@qq.com>
2024-12-05 15:33:46 +0800

    fix: 搜索组件完善

--- packages/components/components/SearchForm.vue
+++ packages/components/components/SearchForm.vue
@@ -1,12 +1,16 @@
 <script setup lang="ts">
 import { FormOption, SearchOption } from '@packages/types'
 import { Input, TreeSelect, InputNumber, Select, Button } from '@packages/components'
-import { ref, unref } from 'vue'
+import { ref, unref, computed } from 'vue'
 const props = defineProps<{
   searchOptions: FormOption[] | SearchOption[]
   handleSearch: (params: Record<string, any>) => void
 }>()
 const searchParams = ref<Record<string, any>>({})
+const showMore = ref<boolean>(false)
+
+const searchFormOptions = computed(() => props.searchOptions.filter((item) => item.search))
+
 function handleClickSearch() {
   const searchData = { ...unref(searchParams) }
   props.searchOptions.forEach((field) => {
@@ -31,12 +35,12 @@ function handleReset() {
 <template>
   <div class="flex flex-wrap bg-gray-50 py-3 px-5 rounded-md">
     <template
-      v-for="searchItem in searchOptions"
+      v-for="(searchItem, index) in searchFormOptions"
       :key="searchItem.field"
     >
       <div
-        class="w-1/3 flex items-center"
-        v-if="searchItem.search"
+        class="w-1/3 flex items-center my-2"
+        v-if="searchItem.search && (showMore || index < 2)"
       >
         <div class="w-32 text-right">{{ searchItem.name }}：</div>
         <div class="w-full flex-grow">
@@ -84,7 +88,12 @@ function handleReset() {
       </div>
     </template>
 
-    <div class="w-1/3 flex px-5">
+    <div
+      class="w-1/3 flex px-5 my-2"
+      :class="{
+        'flex-grow justify-center': showMore
+      }"
+    >
       <Button
         class="mr-2 flex items-center"
         type="primary"
@@ -100,6 +109,21 @@ function handleReset() {
         <div class="i-tabler:zoom-reset w-1em h-1em mr-2"></div>
         重置</Button
       >
+      <a
+        class="ml-2 flex items-center ml-4 text-sm cursor-pointer"
+        @click="showMore = !showMore"
+        v-if="searchFormOptions.length > 2"
+      >
+        <div
+          v-if="showMore"
+          class="i-tabler:chevron-up w-5 h-5 text-gray-6"
+        ></div>
+        <div
+          v-else
+          class="i-tabler:chevron-down w-5 h-5 text-gray-6"
+        ></div>
+        {{ showMore ? '收起更多' : '展开更多' }}
+      </a>
     </div>
   </div>
 </template>
